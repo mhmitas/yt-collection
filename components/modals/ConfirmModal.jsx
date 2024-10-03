c5f6ab3736f45abaf44cd21dbf1ec5c610fc3a62
mhmitas <mahfuzulmitas@gmail.com>
2024-10-03 19:52:12 +0600

    do some style

--- components/modals/ConfirmModal.jsx
+++ components/modals/ConfirmModal.jsx
@@ -13,7 +13,7 @@ const ConfirmModal = ({ message, onConfirm, onCancel }) => {
             className="fixed inset-0 flex items-center justify-center bg-black/40 bg-opacity-75 z-50"
             onClick={handleOverlayClick}
         >
-            <div className="rounded-lg shadow-lg p-6 w-96 bg-background/70 border backdrop-blur-md">
+            <div className="rounded-lg shadow-lg p-6 w-96 bg-background/80 dark:bg-muted/80 backdrop-blur-md">
                 <h2 className="mb-2">Confirm Action</h2>
                 <p className="text-lg font-semibold mb-6">{message}</p>
                 <div className="flex justify-end space-x-4">
